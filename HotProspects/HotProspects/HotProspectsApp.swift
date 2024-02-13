afa68b00ad8a1b368fe543254dffc260e76c3fad
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-13 06:31:35 +0200

    init project

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by OLEKSANDR ISAIEV on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
