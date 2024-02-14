8cb5ec5476229b4e85b3572a58dc75128690bf9c
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-14 05:53:25 +0200

    add example view

---
+++ HotProspects/HotProspects/Study/MultipleSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  MultipleSelectionView.swift
+//  HotProspects
+//
+//  Created by OLEKSANDR ISAIEV on 14.02.2024.
+//
+
+import SwiftUI
+
+struct MultipleSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultipleSelectionView()
+}
