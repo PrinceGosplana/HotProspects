af08ff0c5497972418cdfec03cf572a7eaea731a
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-14 06:05:16 +0200

    replace string with set

--- HotProspects/HotProspects/Study/MultipleSelectionView.swift
+++ HotProspects/HotProspects/Study/MultipleSelectionView.swift
@@ -9,15 +9,15 @@ import SwiftUI
 
 struct MultipleSelectionView: View {
     let users = ["Totoro", "Chany", "BoJack", "Dwarf"]
-    @State private var selection: String?
+    @State private var selection = Set<String>()
     
     var body: some View {
         List(users, id: \.self, selection: $selection) { user in
             Text(user)
         }
         
-        if let selection {
-            Text("You selected \(selection)")
+        if selection.isEmpty == false {
+            Text("You selected \(selection.formatted())")
         }
     }
 }
