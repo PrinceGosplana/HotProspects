a01bab4f0cea8ef57cb9a20542d9070c30f40c03
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-14 06:03:13 +0200

    add example how to select

--- HotProspects/HotProspects/Study/MultipleSelectionView.swift
+++ HotProspects/HotProspects/Study/MultipleSelectionView.swift
@@ -9,10 +9,15 @@ import SwiftUI
 
 struct MultipleSelectionView: View {
     let users = ["Totoro", "Chany", "BoJack", "Dwarf"]
+    @State private var selection: String?
     
     var body: some View {
-        List(users, id: \.self) { user in
+        List(users, id: \.self, selection: $selection) { user in
             Text(user)
+        }
+        
+        if let selection {
+            Text("You selected \(selection)")
         }
     }
 }
