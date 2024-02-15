636807f458af9cc8549eb8e2b91a6dc2369ba8d0
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-15 20:45:37 +0200

    add simple download example

--- HotProspects/HotProspects/Study/ResultView.swift
+++ HotProspects/HotProspects/Study/ResultView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct ResultView: View {
+    @State private var output = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+    
+    func fetchReadings() async {
+        do {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            output = "Found \(readings.count) readings"
+        } catch {
+            print("Download error")
+        }
     }
 }
 
