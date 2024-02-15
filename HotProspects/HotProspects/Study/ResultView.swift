279d7581cb8b8524d200f39046747d0c5d90d6b6
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-15 20:55:20 +0200

    make it work with string

--- HotProspects/HotProspects/Study/ResultView.swift
+++ HotProspects/HotProspects/Study/ResultView.swift
@@ -22,9 +22,15 @@ struct ResultView: View {
             let url = URL(string: "https://hws.dev/readings.json")!
             let (data, _) = try await URLSession.shared.data(from: url)
             let readings = try JSONDecoder().decode([Double].self, from: data)
-            output = "Found \(readings.count) readings"
+            return "Found \(readings.count) readings"
         }
         let result = await fetchTask.result
+        
+        do {
+            output = try result.get()
+        } catch {
+            output = "Error \(error.localizedDescription)"
+        }
     }
 }
 
