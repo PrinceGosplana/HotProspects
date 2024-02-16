d7b1f3279284e7ccf6de91c74437a586ff4c1258
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-16 06:05:23 +0200

    new view for image interpolation example

---
+++ HotProspects/HotProspects/Study/ImageInterpolationView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageInterpolationView.swift
+//  HotProspects
+//
+//  Created by OLEKSANDR ISAIEV on 16.02.2024.
+//
+
+import SwiftUI
+
+struct ImageInterpolationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageInterpolationView()
+}
