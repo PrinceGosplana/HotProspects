74f6fc3d56c249446d94b16d6fcbb99db131110a
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-17 21:04:40 +0200

    context empty view

---
+++ HotProspects/HotProspects/Study/ContextMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  ContextMenuView.swift
+//  HotProspects
+//
+//  Created by OLEKSANDR ISAIEV on 17.02.2024.
+//
+
+import SwiftUI
+
+struct ContextMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContextMenuView()
+}
