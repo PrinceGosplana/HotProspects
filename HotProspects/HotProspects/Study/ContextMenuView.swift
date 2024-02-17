a33820080507489187b3e4ab07e63a606e65ad58
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-17 21:07:54 +0200

    add context menu

--- HotProspects/HotProspects/Study/ContextMenuView.swift
+++ HotProspects/HotProspects/Study/ContextMenuView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct ContextMenuView: View {
+    @State private var backgroundColor = Color.red
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding()
+            .background(backgroundColor)
+        
+        Text("Change Color")
+            .padding()
+            .contextMenu {
+                Button("Red") {
+                    backgroundColor = .red
+                }
+                Button("Green") {
+                    backgroundColor = .green
+                }
+                Button("Blue") {
+                    backgroundColor = .blue
+                }
+            }
     }
 }
 
