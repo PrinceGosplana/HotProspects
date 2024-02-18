a0133c4fce39156464d7ae8fd356e177b4dd1e44
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-18 14:23:37 +0200

    custom row swipe view

---
+++ HotProspects/HotProspects/Study/CustomRowSwipeView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomRowSwipeView.swift
+//  HotProspects
+//
+//  Created by OLEKSANDR ISAIEV on 18.02.2024.
+//
+
+import SwiftUI
+
+struct CustomRowSwipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomRowSwipeView()
+}
