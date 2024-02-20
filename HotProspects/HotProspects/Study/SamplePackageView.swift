487f08bff5a5eab2d31703389ed9f1717c917256
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-20 10:39:02 +0200

    use added package

--- HotProspects/HotProspects/Study/SamplePackageView.swift
+++ HotProspects/HotProspects/Study/SamplePackageView.swift
@@ -5,11 +5,19 @@
 //  Created by OLEKSANDR ISAIEV on 20.02.2024.
 //
 
+import SamplePackage
 import SwiftUI
 
 struct SamplePackageView: View {
+    let possibleNumbers = 1...60
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.formatted()
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(results)
     }
 }
 
