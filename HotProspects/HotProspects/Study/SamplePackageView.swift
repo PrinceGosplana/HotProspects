1090040570fd7cad8bb58f50ff4601b7a7ea33f5
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-20 10:35:04 +0200

    add sample package view

---
+++ HotProspects/HotProspects/Study/SamplePackageView.swift
@@ -0,0 +1,18 @@
+//
+//  SamplePackageView.swift
+//  HotProspects
+//
+//  Created by OLEKSANDR ISAIEV on 20.02.2024.
+//
+
+import SwiftUI
+
+struct SamplePackageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SamplePackageView()
+}
