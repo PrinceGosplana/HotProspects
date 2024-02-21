95a5d58c1ede6eb52ae34e6832ed50e7239feaba
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-21 16:15:26 +0200

    add prospect model

--- HotProspects/HotProspects/Prospect.swift
+++ HotProspects/HotProspects/Prospect.swift
@@ -5,4 +5,17 @@
 //  Created by OLEKSANDR ISAIEV on 21.02.2024.
 //
 
-import Foundation
+import SwiftData
+
+@Model
+class Prospect {
+    var name: String
+    var emailAddress: String
+    var isContacted: Bool
+    
+    init(name: String, emailAddress: String, isContacted: Bool) {
+        self.name = name
+        self.emailAddress = emailAddress
+        self.isContacted = isContacted
+    }
+}
