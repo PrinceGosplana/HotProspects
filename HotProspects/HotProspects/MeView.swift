d5112588d03a752ff18a41818d739374f05cf5a5
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-25 18:02:05 +0200

    add share functional for my view

--- HotProspects/HotProspects/MeView.swift
+++ HotProspects/HotProspects/MeView.swift
@@ -31,6 +31,12 @@ struct MeView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        let image = generateQRCode(from: "\(name)\n\(emailAddress)")
+                        ShareLink(
+                            item: Image(uiImage: image),
+                            preview: SharePreview("My QR Code", image: Image(uiImage: image)))
+                    }
             }
             .navigationTitle("Your code")
         }
